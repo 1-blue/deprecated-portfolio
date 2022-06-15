6863e2b43db8997ade8aa798164235ae0f7005b7
1-blue <1-blue98@naver.com>
2022-06-15 12:43:33 +0900

    Add <details> animation logic
    <detail>에서 스르륵 펼쳐지는 애니메이션 적용 유틸 메서드 추가

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,2 +1,31 @@
 export const combineClassNames = (...classname: string[]) =>
   classname.join(" ");
+
+export const setDetailsHeight = (wrapper: HTMLElement) => {
+  const setHeight = (detail: HTMLDetailsElement, open = false) => {
+    detail.open = open;
+    const { width, height } = detail.getBoundingClientRect();
+    detail.dataset.width = width + "";
+    detail.style.setProperty(
+      open ? `--expanded` : `--collapsed`,
+      `${height}px`
+    );
+  };
+
+  const RO = new ResizeObserver((entries) =>
+    entries.forEach((entry) => {
+      const detail = entry.target as HTMLDetailsElement;
+      const width = detail.dataset.width ? +detail.dataset.width : -1;
+
+      if (width !== entry.contentRect.width) {
+        detail.removeAttribute("style");
+        setHeight(detail);
+        setHeight(detail, true);
+        detail.open = false;
+      }
+    })
+  );
+
+  const details = wrapper.querySelectorAll("details");
+  details.forEach((detail) => RO.observe(detail));
+};
